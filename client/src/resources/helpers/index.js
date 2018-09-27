122bf9300b891dc69828104af0214b16d6e62fad
shmilky <shmilkykup@gmail.com>
2018-09-27 13:24:54 +0300

    Remove not used object helpers (shouldn't be part of the template)

--- client/src/resources/helpers/index.js
+++ client/src/resources/helpers/index.js
@@ -1,8 +1,6 @@
 'use strict';
 
 // import request from './requestWrapper';
-import object from './object';
 import clientLogger from './clientLogger';
 
-module.exports.objectHelpers = object;
 module.exports.logger = clientLogger;
